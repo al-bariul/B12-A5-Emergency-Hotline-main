a93e029f6dc6596d319e2d880b11b4dfd9171324
al-bariul <albariul475@gmail.com>
2025-09-18 15:33:15 +0600

    alert added

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const heart = document.querySelectorAll(".heart");
 const heartIncrease = document.getElementById("heart-increase");
+const call = document.querySelectorAll(".call");
+const callingName = document.getElementById("calling-name");
+const callingNum = document.getElementById("calling-num");
 let num = 1;
 
 for (const h of heart) {
   h.addEventListener("click", function () {
     heartIncrease.innerText = num++;
+  });
+}
+
+const callName = callingName.innerText;
+const callNum = callingNum.innerText;
+console.log(callName, callNum);
+for (const c of call) {
+  c.addEventListener("click", function () {
+    // alert("Calling" + callName + callNum);
+    alert(`calling ${callName} ${callNum}`);
   });
 }
